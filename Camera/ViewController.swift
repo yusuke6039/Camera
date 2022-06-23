9b470ce8c5303e112b1b5ff4acec0bdb8c234c4e
yusuke <yusuke6039@icloud.com>
2022-06-23 22:09:23 +0900

    Initial Commit

---
+++ Camera/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Camera
+//
+//  Created by 新宅雄介 on 2022/06/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
