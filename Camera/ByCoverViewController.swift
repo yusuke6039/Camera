d0ce17bdd9daf1a4a6c19b7db21d7c416f9854fc
yusuke <yusuke6039@icloud.com>
2022-07-13 22:06:42 +0900

    2022/7/13-2

--- Camera/ByCoverViewController.swift
+++ Camera/ByCoverViewController.swift
@@ -7,7 +7,10 @@
 
 import UIKit
 
-class ByCoverViewController: UIViewController {
+class ByCoverViewController: UIViewController,UICollectionViewDelegate,UICollectionViewDataSource {
+    
+    var postarray:[PostData] = []
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -15,6 +18,14 @@ class ByCoverViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return postarray.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell:UICollectionViewCell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        return cell 
+    }
 
     /*
     // MARK: - Navigation
